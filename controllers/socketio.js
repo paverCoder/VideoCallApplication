c79dde7f15ef9bd5bcd5fe48c573486c6fdaefcd
Paver Coder <pavingtechnology@gmail.com>
2023-10-09 21:22:41 +0530

    Intital Code

---
+++ controllers/socketio.js
@@ -0,0 +1,28 @@
+const users = {};
+const chats = {};
+const socketToRoom = new Set();
+const shortid = require("shortid");
+
+// TODO: New Meeting 
+exports.newMeeting = (socket) => {
+
+}
+
+
+// TODO: Join Meet by ID
+exports.joinMeetbyId = (socket, roomID, user) => {
+
+  
+}
+
+// TODO: Leave Meeting 
+exports.leaveMeeting = (socket, roomID, user) => {
+
+}
+
+
+// TODO: send Message to user
+exports.sendMessage = (socket, text, user, roomID) => {
+
+
+}
